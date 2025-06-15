f447265066224c40bb0f08f125d562dde2eca6db
OpusMag <90897827+OpusMag@users.noreply.github.com>
2025-06-15 19:28:55 +0200

    Set it very wide
    Setting this value larger caused more tools to be discovered (unsurprisingly). Didn't think it needed to be that high, but it works and doesn't appear to cause unwanted issues on linux so pushing.

--- internal/discovery/scanner.go
+++ internal/discovery/scanner.go
@@ -39,7 +39,7 @@ func DefaultSafetyLimits() SafetyLimits {
 		MaxConcurrentScans:   3,
 		MaxHelpAttempts:      20,
 		HelpCommandTimeout:   500 * time.Millisecond,
-		MaxExecutablesPerDir: 500,
+		MaxExecutablesPerDir: 10000,
 		DangerousExecutables: []string{
 			"AppHostNameRegistrationVerifier.exe",
 			"SystemSettingsAdminFlows.exe",
