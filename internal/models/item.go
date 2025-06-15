8917dca74b37dbd80430bf20e04978270ab9dc49
OpusMag <90897827+OpusMag@users.noreply.github.com>
2025-06-15 16:04:28 +0200

    Updated items for custom cmds

--- internal/models/item.go
+++ internal/models/item.go
@@ -1,9 +1,12 @@
 package models
 
 type Item struct {
-	Name        string
-	Description string
-	Type        ItemType
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Type        ItemType `json:"type"`
+	Source      string   `json:"source,omitempty"`   // File path or source location
+	Tags        []string `json:"tags,omitempty"`     // Additional metadata
+	Priority    int      `json:"priority,omitempty"` // Search ranking priority
 }
 
 type ItemType int
@@ -12,17 +15,40 @@ const (
 	Command ItemType = iota
 	Flag
 	Hotkey
+	SystemCommand  // Discovered from system PATH
+	CustomCommand  // From user-specified paths
+	UserAlias      // From shell configuration files
+	UserFunction   // Shell functions
+	PackageManager // Commands from package managers
 )
 
 func (t ItemType) String() string {
 	switch t {
 	case Command:
-		return "command"
+		return "builtin-command"
 	case Flag:
 		return "flag"
 	case Hotkey:
 		return "hotkey"
+	case SystemCommand:
+		return "system-command"
+	case CustomCommand:
+		return "custom-command"
+	case UserAlias:
+		return "user-alias"
+	case UserFunction:
+		return "user-function"
+	case PackageManager:
+		return "package-command"
 	default:
 		return "unknown"
 	}
+}
+
+func (i *Item) IsUserDefined() bool {
+	return i.Type == UserAlias || i.Type == UserFunction
+}
+
+func (i *Item) IsSystemDiscovered() bool {
+	return i.Type == SystemCommand || i.Type == CustomCommand
 }
