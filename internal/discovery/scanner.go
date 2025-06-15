16b4a66655decc93632c3fd0ef551965950224ad
OpusMag <90897827+OpusMag@users.noreply.github.com>
2025-06-15 19:21:13 +0200

    Even wider

--- internal/discovery/scanner.go
+++ internal/discovery/scanner.go
@@ -39,7 +39,7 @@ func DefaultSafetyLimits() SafetyLimits {
 		MaxConcurrentScans:   3,
 		MaxHelpAttempts:      20,
 		HelpCommandTimeout:   500 * time.Millisecond,
-		MaxExecutablesPerDir: 100,
+		MaxExecutablesPerDir: 500,
 		DangerousExecutables: []string{
 			"AppHostNameRegistrationVerifier.exe",
 			"SystemSettingsAdminFlows.exe",
